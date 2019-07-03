3e1bac92313550d1b5baa5df62e1a27f44b4ebfc
Opobo Gideon <49255322+opobogideon@users.noreply.github.com>
2019-07-04 01:09:20 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function openMenu()
+{
+    document.getElementById('side-menu').style.display="block";
+    document.getElementById('open').style.display="none";
+    document.getElementById('close').style.display="block";
+}
+
+function closeMenu()
+{
+    document.getElementById('side-menu').style.display="none";
+    document.getElementById('open').style.display="block";
+    document.getElementById('close').style.display="none";
+}
